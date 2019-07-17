bf959249bd55d249439e3d177eadd07afad9583a
RadouaneK <radoinekadari@yahoo.fr>
2019-07-17 17:41:31 +0300

    fixing bags

--- Loss.py
+++ Loss.py
@@ -1,5 +1,5 @@
 import numpy as np 
-import tesnorflow as tf 
+import tensorflow as tf 
 from keras import backend as K
 from utils import model_head, yolo_boxes_to_corners
 
@@ -7,14 +7,14 @@ from utils import model_head, yolo_boxes_to_corners
 def costum_loss(y_true, y_pred):
 
 	#post process model output
-	pred_xy, pred_wh, pred_confidence, pred_class_probs = model_head(y_pred, K.shape(y_pred)[3])
+	pred_xy, pred_wh, pred_confidence, pred_class_probs = model_head(y_pred)
 
 	# retrieve y_true
 	true_xy = y_true[..., :2] 
-    true_wh = y_true[..., 2:4]
+	true_wh = y_true[..., 2:4]
 
-    pred_mins, pred_maxes = yolo_boxes_to_corners(pred_xy, pred_wh)
-    true_mins, true_maxes = yolo_boxes_to_corners(true_xy, true_wh)
+	pred_mins, pred_maxes = yolo_boxes_to_corners(pred_xy, pred_wh)
+	true_mins, true_maxes = yolo_boxes_to_corners(true_xy, true_wh)
 
 	intersect_mins  = K.maximum(pred_mins,  true_mins)
 	intersect_maxes = K.minimum(pred_maxes, true_maxes)
@@ -29,22 +29,22 @@ def costum_loss(y_true, y_pred):
 
 	# compute loss.
 	responsibility_selector = y_true[..., 4]
-
+	
 	xy_diff = K.square(true_xy - pred_xy) * responsibility_selector[..., None]
-	xy_loss = K.reduce_sum(xy_diff, axis=[1, 2, 3, 4])
+	xy_loss = K.sum(xy_diff, axis=[1, 2, 3, 4])
 
 	wh_diff = K.square(K.sqrt(true_wh) - K.sqrt(pred_wh)) * responsibility_selector[..., None]
-	wh_loss = K.reduce_sum(wh_diff, axis=[1, 2, 3, 4])
+	wh_loss = K.sum(wh_diff, axis=[1, 2, 3, 4])
 
 	obj_diff = K.square(iou_scores - pred_confidence) * responsibility_selector
-	obj_loss = K.reduce_sum(obj_diff, axis=[1, 2, 3])
+	obj_loss = K.sum(obj_diff, axis=[1, 2, 3])
 
-	best_iou = K.reduce_max(iou_scores, axis=-1)
-	no_obj_diff = K.square(0 - pred_confidence) * K.to_float(best_iou < 0.6)[..., None] * (1 - responsibility_selector)
-	no_obj_loss = K.reduce_sum(no_obj_diff, axis=[1, 2, 3])
+	best_iou = K.max(iou_scores, axis=-1)
+	no_obj_diff = K.square(0 - pred_confidence) * K.cast(best_iou < 0.6, K.dtype(best_iou))[..., None] * (1 - responsibility_selector)
+	no_obj_loss = K.sum(no_obj_diff, axis=[1, 2, 3])
 
 	clf_diff = K.square(y_true[..., 5:] - pred_class_probs) * responsibility_selector[..., None]
-	clf_loss = K.reduce_sum(clf_diff, axis=[1, 2, 3, 4])
+	clf_loss = K.sum(clf_diff, axis=[1, 2, 3, 4])
 
 	object_coord_scale = 5
 	object_conf_scale = 1
@@ -52,22 +52,9 @@ def costum_loss(y_true, y_pred):
 	object_class_scale = 1
 
 
-total_loss =  object_coord_scale * (xy_loss + wh_loss) + \
+	total_loss =  object_coord_scale * (xy_loss + wh_loss) + \
 		object_conf_scale * obj_loss + noobject_conf_scale * no_obj_loss + \
 		object_class_scale * clf_loss
 
-"""
-if print_loss:
-	total_loss = tf.Print(
-		total_loss, [
-                total_loss, confidence_loss_sum, classification_loss_sum,
-                coordinates_loss_sum
-            ],
-            message='yolo_loss, conf_loss, class_loss, box_coord_loss:')
-"""
+	return total_loss
 
-    return total_loss
-
-with tf.Session() as sess:
-	print(sess.run(cell_grid).shape)
-	print(sess.run(conv_index).shape)
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from keras import backend as K
+import numpy as np 
 
-def model_head(y_pred, anchors):
+anchors = np.array([[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], [16.62, 10.52]])
+
+def model_head(y_pred, anchors = anchors):
 
 	anchors_tensor = K.reshape(K.variable(anchors), [1, 1, 1, len(anchors), 2])
 
@@ -19,24 +22,20 @@ def model_head(y_pred, anchors):
 
 	conv_dims = K.cast(K.reshape(conv_dims, [1, 1, 1, 1, 2]), K.dtype(y_pred))
 
-	box_xy = (K.sigmoid(y_pred[..., :2]) + conv_index) / conv_dims
-	box_wh = (K.exp(y_pred[..., 2:4]) * anchors_tensor) / conv_dims
-	box_confidence = K.sigmoid(y_pred[..., 4:5])
+	box_xy = K.sigmoid(y_pred[..., :2]) + conv_index 
+	box_wh = K.exp(y_pred[..., 2:4]) * anchors_tensor
+	box_confidence = K.sigmoid(y_pred[..., 4])
 	box_class_probs = K.softmax(y_pred[..., 5:])
 
 	return box_xy, box_wh, box_confidence, box_class_probs
+
 
 def yolo_boxes_to_corners(box_xy, box_wh):
     """Convert YOLO box predictions to bounding box corners."""
     box_mins = box_xy - (box_wh / 2.)
     box_maxes = box_xy + (box_wh / 2.)
 
-    return K.concatenate([
-        box_mins[..., 1:2],  # y_min
-        box_mins[..., 0:1],  # x_min
-        box_maxes[..., 1:2],  # y_max
-        box_maxes[..., 0:1]  # x_max
-    ])
+    return box_mins, box_maxes
 
 
 
