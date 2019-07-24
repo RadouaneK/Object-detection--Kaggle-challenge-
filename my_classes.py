1b6a5dd0afc6b0338d65ec9e6ae950bae916854f
RadouaneK <radoinekadari@yahoo.fr>
2019-07-24 17:23:20 +0300

    update my classes

--- my_classes.py
+++ my_classes.py
@@ -1,10 +1,11 @@
 import numpy as np
+import cv2
 import keras
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, list_IDs, labels, batch_size=32, dim=(600,600), n_channels=3,
-                 n_classes=10, shuffle=True):
+                  grid_cells=(19,19), anchor_boxes=5, n_classes=10, shuffle=True):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
@@ -13,6 +14,8 @@ class DataGenerator(keras.utils.Sequence):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
+        self.grid_cells = grid_cells
+        self.anchor_boxes = anchor_boxes
         self.on_epoch_end()
 
     def __len__(self):
@@ -42,14 +45,44 @@ class DataGenerator(keras.utils.Sequence):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
         X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size), dtype=int)
+        y = np.empty((self.batch_size, *self.grid_cells, self.anchor_boxes, 5+self.n_classes))
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
-            # Store sample
-            X[i,] = np.load('data/' + ID + '.npy')
-
+            # Load, preproces and store sample
+            #X[i,] = np.load('data/' + ID + '.npy')
+            img = cv2.imread('data/' + ID)
+            img = cv2.resize(img, self.dim)
+            img /= 255.
+            #img = np.expand_dims(img, 0)
+            X[i,] = img
             # Store class
-            y[i] = self.labels[ID]
+            y[i,] = generate_labels(self.labels[ID], self.grid_cells, 
+                                    self.anchor_boxes, self.n_classes)
+        return X, y 
 
-        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+def generate_labels(image_annotations, grid_cells, box, n_classes):
+
+    y = np.zeros((*grid_cells, box, 5 + n_classes))
+
+    anchor_index  = 1 # an index for the anchor box
+    for annotation in image_annotations:
+        index_x = int(annotation[0] * grid_cells[0])
+        index_y = int(annotation[1] * grid_cells[1])
+
+        if y[index_x, index_y,:, 4] == 0:
+            y[index_x, index_y,:, 0:4]  = annotation[0:4]
+            y[index_x, index_y,:, 4] = 1.
+            y[index_x, index_y,:, 5:] = keras.utils.to_categorical(annotation[4], num_classes=n_classes)[0]
+
+        else:
+            y[index_x, index_y, anchor_index, 0:4]  = annotation[0:4]
+            y[index_x, index_y, anchor_index, 4] = 1.
+            y[index_x, index_y, anchor_index, 5:] = keras.utils.to_categorical(annotation[4], num_classes=n_classes)[0]
+
+            anchor_box += 1 
+    return y 
+
+#y = generate_labels((13,13), 5, 10)
+#print(y.shape)
+
